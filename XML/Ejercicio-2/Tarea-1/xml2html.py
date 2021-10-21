015b1dfd6b493e6c2259ae7500a573b35574b12e
uo271288 <uo271288@uniovi.es>
2021-10-21 19:40:34 +0200

    c

--- XML/Ejercicio-2/Tarea-1/xml2html.py
+++ XML/Ejercicio-2/Tarea-1/xml2html.py
@@ -1,43 +1,112 @@
 import xml.etree.ElementTree as ET
 import codecs
 
+
 def verXML(archivoXML):
     try:
         arbol = ET.parse(archivoXML)
     except IOError:
-        print ('No se encuentra el archivo ', archivoXML)
+        print("No se encuentra el archivo ", archivoXML)
         exit()
     except ET.ParseError:
         print("Error procesando en el archivo XML = ", archivoXML)
         exit()
     raiz = arbol.getroot()
-    file = codecs.open("arbol_genealogico.html","w", "utf-8")
-    text = '<!DOCTYPE HTML><html lang="es-ES"><head><meta name="viewport" content="width=device-width"><meta charset="UTF-8"><meta name="author" content="Alejandro Álvarez Varela" /><meta name="description" content="'+raiz.tag.replace('_', ' ').capitalize()+'" /><meta name="keywords" content="'+raiz.tag.replace('_', ',')+'" /><title>'+raiz.tag.replace('_', ' ').capitalize()+'</title><link rel="stylesheet" type="text/css" href="estilo.css" /></head><body><h1>'+raiz.tag.replace('_', ' ').upper()+"</h1>"
+    file = codecs.open("arbol_genealogico.html", "w", "utf-8")
+    text = (
+        "<!DOCTYPE HTML>"
+        + '<html lang="es-ES">'
+        + "<head>"
+        + '<meta name="viewport" content="width=device-width">'
+        + '<meta charset="UTF-8">'
+        + '<meta name="author" content="Alejandro Álvarez Varela" />'
+        + '<meta name="description" content="'
+        + raiz.tag.replace("_", " ").capitalize()
+        + '" />'
+        + '<meta name="keywords" content="'
+        + raiz.tag.replace("_", ",")
+        + '" />'
+        + "<title>"
+        + raiz.tag.replace("_", " ").capitalize()
+        + "</title>"
+        + '<link rel="stylesheet" type="text/css" href="estilo.css" />'
+        + "</head>"
+        + "<body>"
+        + "<header>"
+        + "<h1>"
+        + raiz.tag.replace("_", " ").upper()
+        + "</h1>"
+        + "</header>"
+    )
 
-    if raiz.text != None:
-        print("Contenido = "    , raiz.text.strip('\n'))
-    else:
-        print("Contenido = "    , raiz.text)
+    text += "<main>"
+    for hijo in raiz.findall(".//"):
+        if hijo.tag == "persona":
+            text += (
+                "<section><h2>"
+                + hijo.attrib.get("nombre")
+                + " "
+                + hijo.attrib.get("apellidos")
+                + "</h2>"
+                + "<p> Fecha de nacimiento: "
+                + hijo.attrib.get("fecha_nacimiento")
+                + "</p>"
+            )
+            if hijo.attrib.get("fecha_fallecimiento") != None:
+                text += (
+                    "<p> Fecha de fallecimiento: "
+                    + hijo.attrib.get("fecha_fallecimiento")
+                    + "</p>"
+                )
 
-    print("Atributos = "    , raiz.attrib)
+        if hijo.tag == "lugar_nacimiento":
+            text += "<p>Lugar de nacimiento: " + hijo.attrib.get("lugar") + "</p>"
+        if hijo.tag == "lugar_fallecimiento":
+            text += "<p>Lugar de fallecimiento: " + hijo.attrib.get("lugar") + "</p>"
+        if hijo.tag == "coordenadas":
+            text += "<p>"
+        if hijo.tag == "latitud":
+            text += hijo.text + ","
+        if hijo.tag == "longitud":
+            text += hijo.text + " "
+        if hijo.tag == "altitud":
+            text += "altitud: " + hijo.text + "m</p>"
+        if hijo.tag == "foto":
+            text += (
+                '<figure><img alt="'
+                + hijo.text
+                + '" src="Multimedia/'
+                + hijo.attrib.get("url")
+                + '" title="'
+                + hijo.text
+                + '"><figcaption>'
+                + hijo.text
+                + "</figcaption></figure>"
+            )
+        if hijo.tag == "video":
+            text += (
+                '<figure><video src="Multimedia/'
+                + hijo.attrib.get("url")
+                + '" controls></video><figcaption>'
+                + hijo.text
+                + "</figcaption></figure>"
+            )
+        if hijo.tag == "comentarios":
+            text += "<p>" + hijo.text + "</p></section>"
 
-    for hijo in raiz.findall('.//'):
-        print("\nElemento = " , hijo.tag)
-        if hijo.text != None:
-            print("Contenido = ", hijo.text.strip('\n'))
-        else:
-            print("Contenido = ", hijo.text)
-        print("Atributos = ", hijo.attrib)
-    text+="</body></html>"
+    text += "</main></body></html>"
     file.write(text)
     file.close()
+
+
 def main():
 
     print(verXML.__doc__)
 
-    miArchivoXML = input('Introduzca un archivo XML = ')
+    miArchivoXML = input("Introduzca un archivo XML = ")
 
     verXML(miArchivoXML)
+
 
 if __name__ == "__main__":
     main()
