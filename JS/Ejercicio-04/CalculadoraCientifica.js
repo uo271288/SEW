ac1b1f1ca58ffc83ea3c681b266e0d51dfdcea78
uo271288 <uo271288@uniovi.es>
2021-11-04 19:47:21 +0100

    c

--- JS/Ejercicio-04/CalculadoraCientifica.js
+++ JS/Ejercicio-04/CalculadoraCientifica.js
@@ -1,128 +1,269 @@
 "use strict";
 class CalculadoraCientifica extends Calculadora {
 
+    constructor() {
+        super();
+        this.mode = "radians";
+        this.changed = false;
+    }
+
     mc() {
         this.memoria = "";
     }
 
     mr() {
-        document.getElementById("resultado").textContent = this.memoria;
+        document.querySelector("body > form > textarea").textContent = this.memoria;
         this.operacion = this.memoria;
         this.operacionMostrada = this.memoria;
     }
 
     ms() {
-        if (!isNaN(document.getElementById("resultado").textContent))
-            this.memoria = document.getElementById("resultado").textContent;
+        if (!isNaN(document.querySelector("body > form > textarea").textContent))
+            this.memoria = document.querySelector("body > form > textarea").textContent;
     }
 
     x2() {
-        this.operacion += "**2";
-        this.operacionMostrada += "^2";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        this.operacion = Math.pow(new Number(this.operacion), 2);
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
+    }
+
+    x3() {
+        this.operacion = Math.pow(new Number(this.operacion), 3);
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     xy() {
-        this.operacion += "**";
+        this.operando1 = new Number(this.operacion)
+        this.operacion = "";
+        this.operador = "**"
         this.operacionMostrada += "^";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        Math.
+            document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     sin() {
-        this.operacion += "Math.sin(";
-        this.operacionMostrada += "sin(";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        this.operacion = Math.sin(new Number(this.operacion));
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     cos() {
-        this.operacion += "Math.cos(";
-        this.operacionMostrada += "cos(";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        this.operacion = Math.cos(new Number(this.operacion));
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     tan() {
-        this.operacion += "Math.tan(";
-        this.operacionMostrada += "tan(";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        this.operacion = Math.tan(new Number(this.operacion));
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
+    }
+
+    asin() {
+        this.operacion = Math.asin(new Number(this.operacion));
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
+    }
+
+    acos() {
+        this.operacion = Math.acos(new Number(this.operacion));
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
+    }
+
+    atan() {
+        this.operacion = Math.atan(new Number(this.operacion));
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     sqrt() {
-        this.operacion += "Math.sqrt(";
-        this.operacionMostrada += "√(";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        this.operacion = Math.sqrt(new Number(this.operacion));
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
+    }
+
+    inv() {
+        this.operacion = 1 / new Number(this.operacion);
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
+    }
+
+    cbrt() {
+        this.operacion = Math.cbrt(new Number(this.operacion));
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     e10x() {
-        this.operacion += "10**";
-        this.operacionMostrada += "10^";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        this.operacion = Math.pow(10, new Number(this.operacion));
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     log10() {
-        this.operacion += "Math.log10(";
-        this.operacionMostrada += "log(";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        this.operacion = Math.log10(new Number(this.operacion));
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
+    }
+
+    ln() {
+        this.operacion = Math.log(new Number(this.operacion));
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     ex() {
-        this.operacion += "Math.exp(";
-        this.operacionMostrada += "e^";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        this.operacion = Math.exp(new Number(this.operacion));
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     mod() {
-        this.operacion += "%";
-        this.operacionMostrada += " Mod ";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        this.operando1 = new Number(this.operacion)
+        this.operacion = "";
+        this.operador = "%"
+        this.operacionMostrada += "Mod";
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     pi() {
         this.operacion += Math.PI;
         this.operacionMostrada += "π";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
+    }
+
+    change() {
+        if (!this.changed) {
+            let b1 = document.createElement("button");
+            b1.setAttribute("type", "button");
+            b1.setAttribute("onclick", "calculadora.x3()");
+            b1.innerHTML = "x<sup>3</sup>";
+            document.querySelector("body > form > button:nth-child(7)").replaceWith(b1);
+
+
+            let b2 = document.createElement("button");
+            b2.setAttribute("type", "button");
+            b2.setAttribute("onclick", "calculadora.cbrt()");
+            b2.innerHTML = "&#8731;";
+            document.querySelector("body > form > button:nth-child(8)").replaceWith(b2);
+
+            let b3 = document.createElement("button");
+            b3.setAttribute("type", "button");
+            b3.setAttribute("onclick", "calculadora.asin()");
+            b3.innerHTML = "sin<sup>-1</<sup>";
+            document.querySelector("body > form > button:nth-child(9)").replaceWith(b3);
+
+            let b4 = document.createElement("button");
+            b4.setAttribute("type", "button");
+            b4.setAttribute("onclick", "calculadora.acos()");
+            b4.innerHTML = "cos<sup>-1</<sup>";
+            document.querySelector("body > form > button:nth-child(10)").replaceWith(b4);
+
+            let b5 = document.createElement("button");
+            b5.setAttribute("type", "button");
+            b5.setAttribute("onclick", "calculadora.atan()");
+            b5.innerHTML = "tan<sup>-1</<sup>";
+            document.querySelector("body > form > button:nth-child(11)").replaceWith(b5);
+
+            let b6 = document.createElement("button");
+            b6.setAttribute("type", "button");
+            b6.setAttribute("onclick", "calculadora.inv()");
+            b6.innerHTML = "1/x";
+            document.querySelector("body > form > button:nth-child(12)").replaceWith(b6);
+
+            let b7 = document.createElement("button");
+            b7.setAttribute("type", "button");
+            b7.setAttribute("onclick", "calculadora.ln()");
+            b7.innerHTML = "ln";
+            document.querySelector("body > form > button:nth-child(14)").replaceWith(b7);
+            this.changed = true;
+        }
+        else {
+            let b1 = document.createElement("button");
+            b1.setAttribute("type", "button");
+            b1.setAttribute("onclick", "calculadora.x2()");
+            b1.innerHTML = "x<sup>2</sup>";
+            document.querySelector("body > form > button:nth-child(7)").replaceWith(b1);
+
+
+            let b2 = document.createElement("button");
+            b2.setAttribute("type", "button");
+            b2.setAttribute("onclick", "calculadora.xy()");
+            b2.innerHTML = "x<sup>y</sup>";
+            document.querySelector("body > form > button:nth-child(8)").replaceWith(b2);
+
+            let b3 = document.createElement("button");
+            b3.setAttribute("type", "button");
+            b3.setAttribute("onclick", "calculadora.sin()");
+            b3.innerHTML = "sin";
+            document.querySelector("body > form > button:nth-child(9)").replaceWith(b3);
+
+            let b4 = document.createElement("button");
+            b4.setAttribute("type", "button");
+            b4.setAttribute("onclick", "calculadora.cos()");
+            b4.innerHTML = "cos";
+            document.querySelector("body > form > button:nth-child(10)").replaceWith(b4);
+
+            let b5 = document.createElement("button");
+            b5.setAttribute("type", "button");
+            b5.setAttribute("onclick", "calculadora.tan()");
+            b5.innerHTML = "tan";
+            document.querySelector("body > form > button:nth-child(11)").replaceWith(b5);
+
+            let b6 = document.createElement("button");
+            b6.setAttribute("type", "button");
+            b6.setAttribute("onclick", "calculadora.sqrt()");
+            b6.innerHTML = "&#x221A;";
+            document.querySelector("body > form > button:nth-child(12)").replaceWith(b6);
+
+            let b7 = document.createElement("button");
+            b7.setAttribute("type", "button");
+            b7.setAttribute("onclick", "calculadora.log10()");
+            b7.innerHTML = "log";
+            document.querySelector("body > form > button:nth-child(14)").replaceWith(b7);
+
+            this.changed = false;
+        }
+    }
+
+    invert() {
+        this.operacion = - new Number(this.operacion)
+        this.operacionMostrada = this.operacion
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     e() {
         this.operacion += Math.E;
         this.operacionMostrada += "e";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
-    }
-
-    abs() {
-        this.operacion += "Math.abs(";
-        this.operacionMostrada += "abs(";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
-    }
-
-    cbrt() {
-        this.operacion += "Math.cbrt(";
-        this.operacionMostrada += "∛(";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     ln() {
-        this.operacion += "Math.log(";
-        this.operacionMostrada += "ln(";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        this.operacion = Math.log(new Number(this.operacion));
+        this.operacionMostrada = this.operacion;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     pr() {
-        this.operacion += ")";
-        this.operacionMostrada += ")";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        let aux = this.operacionMostrada
+        this.igual()
+        this.operacionMostrada = aux + ")"
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     pl() {
-        this.operacion += "(";
         this.operacionMostrada += "(";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     retroceso() {
         var str = this.operacion.substring(0, this.operacion.length - 1);
         this.operacion = str;
         str = this.operacionMostrada.substring(0, this.operacionMostrada.length - 1);
-        document.getElementById("resultado").textContent = str;
+        document.querySelector("body > form > textarea").textContent = str;
         this.operacionMostrada = str;
     }
 }
