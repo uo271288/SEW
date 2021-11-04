bd0b8859ee873b013bfe6f01166d00119bbf9af3
uo271288 <uo271288@uniovi.es>
2021-11-04 02:56:18 +0100

    c

--- JS/Ejercicio-03/CalculadoraBasica.js
+++ JS/Ejercicio-03/CalculadoraBasica.js
@@ -5,79 +5,113 @@ class Calculadora {
         this.operacion = "";
         this.operacionMostrada = "";
         this.memoria = "";
-        this.resuelto = false;
+        this.resuelto = false
+
+        document.addEventListener("keydown", (event) => {
+            const keyName = event.key.replace(/[^\d.\-\/\*\+]/g, '');
+            if (this.resuelto && !["*", "/", "-", "+"].some(el => keyName.includes(el))) {
+                document.querySelector("body > form > textarea").textContent = "";
+                this.resuelto = false;
+                this.operacion = "";
+                this.operacionMostrada = ""
+            }
+            if (["*", "/", "-", "+"].some(el => keyName.includes(el))) {
+                this.resuelto = false;
+            }
+            document.querySelector("body > form > textarea").textContent += keyName;
+            this.operacion += keyName
+            this.operacionMostrada += keyName
+            if (event.code.match("Enter")) {
+                this.igual();
+                this.resuelto = true;
+            }
+        });
     }
 
     digitos(arg) {
         if (this.resuelto) {
-            document.getElementById("resultado").textContent = "";
+            document.querySelector("body > form > textarea").textContent = "";
             this.resuelto = false;
+            this.operacion = "";
+            this.operacionMostrada = ""
         }
         this.operacion += arg;
         this.operacionMostrada += arg;
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     punto() {
         this.operacion += ".";
         this.operacionMostrada += ".";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     suma() {
         this.operacion += "+";
         this.operacionMostrada += "+";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        this.resuelto = false;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
-    resta(resta) {
+    resta() {
         this.operacion += "-";
         this.operacionMostrada += "-";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
-    }
-    
-    mult(mult) {
-        this.operacion += "*";
-        this.operacionMostrada += "*";
-        document.querySelector("body > div > textarea").textContent = this.operacionMostrada;
+        this.resuelto = false;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
-    div(div) {
+    mult() {
+        this.operacion += "*";
+        this.operacionMostrada += "*";
+        this.resuelto = false;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
+    }
+
+    div() {
         this.operacion += "/";
         this.operacionMostrada += "/";
-        document.getElementById("resultado").textContent = this.operacionMostrada;
+        this.resuelto = false;
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
     }
 
     igual() {
-        this.operacion = eval(this.operacion);
-        this.operacionMostrada = eval(this.operacion);
-        document.getElementById("resultado").textContent = this.operacionMostrada;
-        this.resuelto = false;
+        this.operacion = Number(eval(this.operacion));
+        this.operacionMostrada = Number(eval(this.operacion));
+        document.querySelector("body > form > textarea").textContent = this.operacionMostrada;
+        this.resuelto = true;
     }
 
     borrar() {
-        document.getElementById("resultado").textContent = "";
+        document.querySelector("body > form > textarea").textContent = "";
         this.operacion = "";
         this.operacionMostrada = "";
     }
 
     mMenos() {
-        if (!isNaN(document.getElementById("resultado").textContent))
-            this.memoria = eval(this.memoria + "-" + document.getElementById("resultado").textContent);
+        if (!Number(document.querySelector("body > form > textarea").textContent).isNaN) {
+            this.memoria = Number(eval(this.memoria + "-" + document.querySelector("body > form > textarea").textContent));
+            this.operacion = "";
+            this.operacionMostrada = "";
+        }
     }
 
     mMas() {
-        if (!isNaN(document.getElementById("resultado").textContent))
-            this.memoria = eval(this.memoria + "+" + document.getElementById("resultado").textContent);
+        if (!Number(document.querySelector("body > form > textarea").textContent).isNaN) {
+            this.memoria = Number(eval(this.memoria + "+" + document.querySelector("body > form > textarea").textContent));
+            this.operacion = "";
+            this.operacionMostrada = "";
+        }
     }
 
     mrc() {
         var resultado = this.memoria;
-        document.getElementById("resultado").textContent = resultado;
+        document.querySelector("body > form > textarea").textContent = resultado;
         this.operacion = resultado;
         this.operacionMostrada = resultado;
         this.memoria = "";
+        this.resuelto = true
     }
+
 }
 
 var calculadora = new Calculadora();
