05a7f143358f4a0d093e702c9372e538a3c8fba0
uo271288 <uo271288@uniovi.es>
2021-11-18 11:51:14 +0100

    c

--- JS/Ejercicio-13/Tarea-2/MapaGeoJSON.js
+++ JS/Ejercicio-13/Tarea-2/MapaGeoJSON.js
@@ -1,34 +1,29 @@
 "use strict";
 class GeoJSON {
 
-   initMap() {
+    initMap() {
         document.body.querySelector("article").appendChild(document.createElement("section"))
-        var centro = {lat: 43.3672702, lng: -5.8502461};
-        this.map = new google.maps.Map(document.querySelector("body > article > section"),{
-        zoom: 8,
-        center:centro,
-        mapTypeId: google.maps.MapTypeId.SATELLITE
+        var centro = { lat: 43.3672702, lng: -5.8502461 };
+        this.map = new google.maps.Map(document.querySelector("body > article > section"), {
+            zoom: 8,
+            center: centro,
+            mapTypeId: google.maps.MapTypeId.SATELLITE
         });
-        
+
     }
 
-   load(files) {
-       var archivo = files[0];         
-       var lector = new FileReader();
-       var oThis = this;
-       lector.onload = function(event) {
-       var text = lector.result;
-       var obj = JSON.parse(text);
-       oThis.map.data.addGeoJson(obj);
-            }
-            
-            lector.readAsText(archivo);
-        
+    load(files) {
+        var archivo = files[0];
+        var lector = new FileReader();
+        var oThis = this;
+        lector.onload = function (event) {
+            var text = lector.result;
+            var obj = JSON.parse(text);
+            oThis.map.data.addGeoJson(obj);
+        }
+
+        lector.readAsText(archivo);
+
     }
 }
 var verJson = new GeoJSON();
-
-
-  
-
-
