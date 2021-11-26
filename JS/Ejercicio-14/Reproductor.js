da8f55f9db89f0fdfd054efab510a878d3e5d90f
uo271288 <uo271288@uniovi.es>
2021-11-26 19:18:50 +0100

    c

--- JS/Ejercicio-14/Reproductor.js
+++ JS/Ejercicio-14/Reproductor.js
@@ -27,12 +27,13 @@ class Reproductor {
             track,
             track = video.addTextTrack("captions", "English", "en");
         track.mode = "showing";
-        track.addCue(new VTTCue(0, 26, "* Music *"));
-        track.addCue(new VTTCue(27, 28, "Tequila"));
-        track.addCue(new VTTCue(29, 68, "* Music *"));
-        track.addCue(new VTTCue(69, 70, "Tequila"));
-        track.addCue(new VTTCue(71, 100, "* Music *"));
-        track.addCue(new VTTCue(101, 102, "Tequila"));
+        track.addCue(new VTTCue(0, 2, "La vi con otra paloma"));
+        track.addCue(new VTTCue(2.5, 4, "* inspira lastimosamente * me wa matar"));
+        track.addCue(new VTTCue(5, 6.5, "Me wa matar [lenguaje ininteligible] wee"));
+        track.addCue(new VTTCue(7, 8, "Wee"));
+        track.addCue(new VTTCue(8.5, 9.5, "Wee"));
+        track.addCue(new VTTCue(10.5, 11.5, "Me wa matar wee"));
+        track.addCue(new VTTCue(12, 13, "Weeee"));
     }
 
 
